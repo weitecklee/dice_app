d136acbc2397f2dd150a01924c061b09feed6929
arkteck <arkteck@gmail.com>
2024-07-10 09:56:54 -0400

    style text

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -17,7 +17,13 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: const Center(
-        child: Text('Hello world'),
+        child: Text(
+          'Hello world',
+          style: TextStyle(
+            fontSize: 28.0,
+            color: Colors.white,
+          ),
+        ),
       ),
     );
   }
